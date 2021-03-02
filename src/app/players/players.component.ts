948fba3aa6315786ede516b1d4272bbe6083a8d2
ifratmitul <ifrat.mitul@hotmail.com>
2021-03-02 19:12:01 +0600

    all players and tournament added

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -9,18 +9,30 @@ export class PlayersComponent implements OnInit {
 
 
   playerList = [
-    { id:0, name:'Mitul', active: true, position:'CB/RB' },
-    { id:1, name:'H.Kabir', active: true, position:'CAM/CM/Wing' },
-    { id:2, name:'Gazi', active: true, position:'CB/CDM' },
-    { id:3, name:'Shimu', active: true, position:'CB/CDM' },
-    { id:4, name:'Arefin', active: true, position:'CM/CDM' },
-    { id:5, name:'Akash', active: true, position:'ST/Wing' },
-    { id:6, name:'Asir', active: true, position:'ST/Wing' },
-    { id:7, name:'Rashad', active: true, position:'RB/LB' },
-    { id:8, name:'Tanzib', active: true, position:'Wing/CM' },
-    { id:9, name:'Ishtiaq', active: true, position:'LB' },
-    { id:10, name:'Fahim', active: true, position:'Defense' },
-    { id:11, name:'Mahdeen', active: true, position:'Forward' },
+    { honorableMention: '2017 - 2021', id:0, name:'Mitul', active: true, position:'CB/RB' },
+    { honorableMention: '2017 - 2021', id:1, name:'H.Kabir', active: true, position:'CAM/CM/Wing' },
+    { honorableMention: '2020 - 2021', id:2, name:'Gazi', active: true, position:'CB/CDM' },
+    { honorableMention: '2019 - 2021', id:3, name:'Shimu', active: true, position:'CB/CDM' },
+    { honorableMention: '2018 - 2021', id:4, name:'Arefin', active: true, position:'CM/CDM' },
+    { honorableMention: '2018 - 2021', id:5, name:'Akash', active: true, position:'ST/Wing' },
+    { honorableMention: '2020 - 2021', id:6, name:'Asir', active: true, position:'ST/Wing' },
+    { honorableMention: '2020 - 2021', id:7, name:'Rashad', active: true, position:'RB/LB' },
+    { honorableMention: 'VPG Bangladesh Pro League S3', id:8, name:'Tanzib', active: true, position:'Wing/CM' },
+    { honorableMention: 'VPG Bangladesh Pro League S3', id:9, name:'Ishtiaq', active: true, position:'LB' },
+    { honorableMention: 'VPG Bangladesh Pro League S3', id:10, name:'Fahim', active: true, position:'Defense' },
+    { honorableMention: '2018 - 2021', id:10, name:'Azan', active: true, position:'LB/CDM' },
+    { honorableMention: 'VPG Bangladesh Pro League S3', id:10, name:'Aiman', active: true, position:'CAM' },
+    
+
+    { honorableMention: 'VPG Asian Champions League Squad', id:11, name:'Tamkin', active: true, position:'CB' },
+    { honorableMention: 'VPG Asian Champions League Squad', id:11, name:'Ishmam', active: true, position:'CB' },
+    { honorableMention: 'VPG Asian Champions League Squad', id:11, name:'Polok', active: true, position:'LB' },
+    { honorableMention: 'VPG Asian Champions League, VPG Bangladesh Pro League S2', id:11, name:'Mahdi Anan', active: true, position:'Midfield/GK' },
+    { honorableMention: 'VPG Asian Pro League Squad', id:10, name:'Tamim', active: true, position:'RW' },
+    { honorableMention: 'VPG Asian Pro League Squad', id:10, name:'Saeem', active: true, position:'CB' },
+    { honorableMention: 'VPG Asian Pro League Squad', id:10, name:'Shikky', active: true, position:'CM' },
+    { honorableMention: 'VPG Asian Pro League Squad', id:10, name:'Masrur', active: true, position:'CDM' },
+    { honorableMention: 'VPG Asian Pro League Squad', id:10, name:'Jholok', active: true, position:'ST' },
 
 ]
 
